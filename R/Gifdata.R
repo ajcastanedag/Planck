806d4f79b0669588e1ec8f449c82f6397c350da9
Antonio Jose Castaneda Gomez <antonio.castanedag@gmail.com>
2019-11-27 18:12:49 +0100

    Working!

--- R/Gifdata.R
+++ R/Gifdata.R
@@ -3,6 +3,7 @@
 #' This function says hello to new EAGLES
 #'
 #'@param Planck_Graph dataframe with planck data
+#'@param Time Fade time
 #'
 #'@return Nothing, just say Hello
 #'
@@ -16,15 +17,14 @@
 #' @export
 #'
 
-Planck_Gif <- function(Planck_Graph){
-              Planck_Graph + transition_time(Temperature) +
-              ggtitle("Planck´s Spectrum of Radiation") +
-              shadow_wake(wake_length = 0.01, alpha = FALSE)+
-              dark_theme_gray() +
-              enter_fade() +
-              exit_fade()
+GifPlanckData <- function(Planck_Graph, Time = 0.01){
+  Planck_Gif <- Planck_Graph + transition_time(Temperature) +
+                ggtitle("Planck´s Spectrum of Radiation") +
+                shadow_wake(wake_length = Time, alpha = TRUE)+
+                dark_theme_gray() +
+                enter_fade() +
+                exit_fade()
+  Planck_Gif <<- Planck_Gif
 }
-
-
 
 
--- R/Plotpdata.R
+++ R/Plotpdata.R
@@ -12,7 +12,7 @@
 #' @export
 #'
 
-Planck_Plot <- function(PlankData_F){
+PlotPlanckData <- function(PlankData_F){
     Planck_Graph <- ggplot(PlankData_F, aes(Wavelength, Radiance, color = Temperature)) +
                     geom_point(size = 0.5)+
                     #geom_line(data =WienData, aes(Wavelength, Radiance_Max), color = "white") +

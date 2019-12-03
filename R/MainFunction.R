3911b4c1538ee5c2927e1a6de7102275c3e4e2e9
Antonio Jose Castaneda Gomez <antonio.castanedag@gmail.com>
2019-12-03 09:57:39 +0100

    Comentaries

--- R/MainFunction.R
+++ R/MainFunction.R
@@ -11,19 +11,26 @@
 #' @return Nothing, just create dataframes
 #' @export
 DefinePlanckData <- function(Tmin=1000,Tmax=10000,Tinterv=1000,Wmin=0.01,Wmax=3000,Winterv=1){
+                            #Call for the variables definition like speed of light and other constants
                             Define_Variables()
+                            #Create empty dataframe to store all the inforation
                             PlankData_F <- data.frame()
-
+                            #For loop to create the data
                             for(Kk in seq(from=Tmin, to=Tmax, by=Tinterv)){
+                                          #Create temporal dataframe with single temperature (KK) dataframe using Planck law
                                           data <- data.frame("Wavelength" = Xval_nm,
                                                              "Radiance" = Plank_Rad(Xval_nm,Kk),
                                                              "Temperature" = Kk,
                                                              "Lam_Mx"=(Wein_C/Kk),
                                                              "RMax" = Plank_Rad((Wein_C/Kk),Kk))
+                                          #Assign temporal dataframe to a new dataframe
                                           Planck_Tmp <- assign(paste("Planck",Kk,sep="_Kk"),data)
+                                          #Rebind just created data into the global dataframe taht will be exported
                                           PlankData_F <- rbind(PlankData_F, Planck_Tmp)
                             }
+                            #Export rbinded data into a global variable called PlankData_F
                             PlankData_F <<- PlankData_F
+                            #Remove temporal data created that will not be used any more
                             rm(list=ls())
 }
 
